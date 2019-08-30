4fc8a31aa59ab6dfe7b04612ea1567586cfddc85
Chris Grabosky <chris@grabosky.net>
2019-08-29 21:55:45 -0400

    MVP

--- Stitch-Hosting/conf/conf.js
+++ Stitch-Hosting/conf/conf.js
@@ -31,3 +31,13 @@ function makeRandomID(length) {
         return false;
     }
  }
+
+ Date.prototype.yyyymmdd = function() {
+    var mm = this.getMonth() + 1; // getMonth() is zero-based
+    var dd = this.getDate();
+
+    return [(mm>9 ? '' : '0') + mm,
+            (dd>9 ? '' : '0') + dd,
+            this.getFullYear().toString().substr(-2)
+            ].join('/');
+};
